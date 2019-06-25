f8fb1e2373acd948adeffd634a00125e5f5e3ad2
Fabio de Stefani <fabiostefani@gmail.com>
2019-06-25 06:35:36 -0300

    Update config.js

--- node-str/src/config.js
+++ node-str/src/config.js
@@ -2,7 +2,7 @@ global.SALT_KEY = 'f5b99242-6504-4ca3-90f2-05e78e5761ef';
 global.EMAIL_TMPL = 'Olá, <strong>{0}</strong>, seja bem ao fabiostefani.io';
 
 module.exports = {
-    connectionString: 'mongodb+srv://fabiostefani:fabiostefani@fabiostefani-mongodb-8xxdq.mongodb.net/test?retryWrites=true&w=majority',
-    sendgridkey: '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',
-    containerConnectionString: 'DefaultEndpointsProtocol=https;AccountName=cursonodejs;AccountKey=z9X9/nWNJuvA82lHv3Rp783SO5tCeQsujOwYbkBh4T6ZCkYFRt79HmIZXAit7T0tIcxefw9O2Nir5MHJTUkVgw==;EndpointSuffix=core.windows.net'
+    connectionString: '<SUA_CONNECTION>',
+    sendgridkey: '<SUA_API>',
+    containerConnectionString: '<SUA_CONNECTION>'
 }
